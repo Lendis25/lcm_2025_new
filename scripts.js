c22373a15c3b43fd0fc0cfe441bb3386bc0a9194
Lendis <l_endis@hotmail.com>
2025-04-08 16:26:10 -0400

    Inicializar proyecto con npm y Vite

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function verDesempenoAlumno() {
+    const rut = document.getElementById("rut-alumno").value;
+    // Aquí deberías obtener y mostrar los datos de desempeño del alumno según el RUT
+    alert("Mostrar desempeño de alumno con RUT: " + rut);
+}
+
+function verDesempenoCurso() {
+    const curso = document.getElementById("curso-seleccionado").value;
+    // Aquí deberías obtener y mostrar los datos del curso seleccionado
+    alert("Mostrar desempeño del curso: " + curso);
+}
